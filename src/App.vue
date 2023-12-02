02d9aeba624e0701abed47b74652c1e42e1b8f0b
Djamil <dsmoonweb@gmail.com>
2023-12-02 01:41:33 +0100

    create components, Add user Form and event handling with personalize text

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { ref } from 'vue'
+import MainLayout from './components/Layout/MainLayout.vue'
+import MainNav from './components/Layout/MainNav.vue'
+import BasicPage from './components/Page/BasicPage.vue'
+
+let NavItemsArray = ref([
+  {
+    name: 'Home',
+    link: '#',
+    target: '_self',
+    class: 'nav-link'
+  },
+  {
+    name: 'Services',
+    link: '#',
+    target: '_self',
+    class: 'nav-link'
+  },
+  {
+    name: 'Contact',
+    link: '#',
+    target: '_self',
+    class: 'nav-link'
+  }
+])
+let UserNavItemsArray = ref([
+  {
+    name: 'Settings',
+    link: '#',
+    target: '_self'
+  },
+  {
+    name: 'Profile',
+    link: '#',
+    target: '_self'
+  }
+])
+</script>
+
+<template>
+  <main-layout>
+    <template v-slot:header>
+      <main-nav :navItems="NavItemsArray" :userNavItems="UserNavItemsArray"> </main-nav>
+    </template>
+    <basic-page></basic-page>
+    
+  </main-layout>
+
+  <main></main>
+</template>
+
+<style scoped></style>
